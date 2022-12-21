a1397cc448c3b92d013de0a56624bb639fa2ef82
Jinyoung Jang <jyjang@uengine.org>
2022-12-21 17:06:16 +0900

    commit

---
+++ inventory/src/main/java/labshopmsareqresp/domain/DecreaseStockCommand.java
@@ -0,0 +1,15 @@
+package labshopmsareqresp.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Date;
+import lombok.Data;
+
+
+@Data
+public class DecreaseStockCommand {
+
+        private Integer qty;
+
+
+}
